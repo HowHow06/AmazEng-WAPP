eb5fbd756ed7062cb50e99fee5c57c991b7ac053
Howard <howard_bb@hotmail.com>
2022-05-08 21:36:15 +0800

    a demo push

--- AmazEng-WAPP/Migrations/Configuration.cs
+++ AmazEng-WAPP/Migrations/Configuration.cs
@@ -28,7 +28,6 @@
             //  This method will be called after migrating to the latest version.
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
-
             InitialiseTagData(context);
             InitialiseIdiomData(context);
 
