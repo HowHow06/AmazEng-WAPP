cdb0e46cbb26ce2d588d0667d574137fc8f62cd9
Howard <howard_bb@hotmail.com>
2022-05-09 15:52:01 +0800

    code refactor

---
+++ AmazEng-WAPP/App_Data/RemoveIdiomAdminDataQuery.sql
@@ -0,0 +1,6 @@
+﻿DELETE FROM [dbo].Admins;
+DELETE FROM [dbo].AdminRoles;
+
+
+DBCC CHECKIDENT ('Admins', RESEED, 0);
+DBCC CHECKIDENT ('AdminRoles', RESEED, 0);
--- AmazEng-WAPP/Class/Utils/Util.cs
+++ AmazEng-WAPP/Class/Utils/Util.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
+using System.Web.UI;
 
-namespace AmazEng_WAPP.Utils
+namespace AmazEng_WAPP.Class.Utils
 {
     public class Util
     {
@@ -14,6 +17,35 @@ namespace AmazEng_WAPP.Utils
             string sFile = System.IO.Path.Combine(sCurrentDirectory, relativePath);
             string sFilePath = Path.GetFullPath(sFile);
             return sFilePath;
+        }
+
+
+        public static void ShowAlert(Page page, string message)
+        {
+            ScriptManager.RegisterStartupScript(page, page.GetType(), "alertFunction", $"alert('{message}');", true);
+        }
+
+        public static void LogConsole(Page page, string message)
+        {
+            ScriptManager.RegisterStartupScript(page, page.GetType(), "alertFunction", $"console.log('{message}');", true);
+        }
+
+        public static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
