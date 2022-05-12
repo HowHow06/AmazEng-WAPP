da2e034244a1fe8413392f309c6b387cd5ca26c0
Callmeks <kskingggg@gmail.com>
2022-05-12 20:34:48 +0800

    message successfully add to database

---
+++ AmazEng-WAPP/Migrations/202205121232278_modifyMemberIdToNullableInMessageTable.cs
@@ -0,0 +1,16 @@
+﻿namespace AmazEng_WAPP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class modifyMemberIdToNullableInMessageTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
