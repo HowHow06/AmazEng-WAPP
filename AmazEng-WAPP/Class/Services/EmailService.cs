afd7ceeb8dad06324336187b768674e0efdaaefc
Howard <howard_bb@hotmail.com>
2022-05-19 02:46:40 +0800

    update: setup email mail trap

---
+++ AmazEng-WAPP/Class/Services/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace AmazEng_WAPP.Class.Services
+{
+    public class EmailService
+    {
+        static string smtpEndPoint = ConfigurationManager.AppSettings["emailServiceProviderSmtp"];
+        static string smtpPort = ConfigurationManager.AppSettings["emailServiceProviderPort"];
+        static string smtpUsername = ConfigurationManager.AppSettings["emailServiceUserName"];
+        static string smtpPassword = ConfigurationManager.AppSettings["emailServicePassword"];
+
+
+        public static void SendEmail(MimeMessage email)
+        {
+
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Connect(smtpEndPoint,
+                    Convert.ToInt32(smtpPort),
+                    SecureSocketOptions.StartTls);
+                smtp.Authenticate(
+                   smtpUsername,
+                    smtpPassword
+                    );
+                smtp.Send(email);
+                smtp.Disconnect(true);
+            }
+        }
+    }
+}
--- AmazEng-WAPP/Forgot.aspx.cs
+++ AmazEng-WAPP/Forgot.aspx.cs
@@ -1,6 +1,16 @@
-﻿using System;
+﻿using AmazEng_WAPP.Class.Auth;
+using AmazEng_WAPP.Class.Services;
+using AmazEng_WAPP.Class.Utils;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,6 +22,27 @@ namespace AmazEng_WAPP
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string emailAddress = txtEmail.Text;
+            string newPassword = "asdasdasdsa";
+            // create email message
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(ConfigurationManager.AppSettings["emailServiceFromEmail"]));
+            email.To.Add(MailboxAddress.Parse(emailAddress));
+            email.Subject = "Password Recovery";
+            email.Body = new TextPart(TextFormat.Html)
+            {
+                Text = $"" +
+                $"<div style='font-family: Arial, Helvetica, sans-serif;'>" +
+                $"<h1>Password Recovery</h1><p>Dear member, your password has been reset successfully.</p>" +
+                $"<p>Your new password is:<u>{newPassword}</p></u>" +
+                $"</div>"
+            };
+
+            EmailService.SendEmail(email);
         }
     }
 }
