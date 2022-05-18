2c32733a58d460be982c2312aa76ae470ff9c8fb
Howard <howard_bb@hotmail.com>
2022-05-18 17:00:10 +0800

    update: refind login logic, check deleted at

--- AmazEng-WAPP/Site.Master.cs
+++ AmazEng-WAPP/Site.Master.cs
@@ -1,4 +1,6 @@
 ﻿using AmazEng_WAPP.Class.Utils;
+using AmazEng_WAPP.DataAccess;
+using AmazEng_WAPP.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,25 @@ namespace AmazEng_WAPP
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Request.IsAuthenticated)
+            {
+                return;
+            }
+            Member currentMember;
+            string username = HttpContext.Current.User.Identity.Name;
+
+            AmazengContext db = new AmazengContext();
+            currentMember = db.Members.Where(m => m.Username == username).First();
+
+            if (currentMember is null || currentMember.DeletedAt != null)
+            {
+                // deleted user or invalid user
+                FormsAuthentication.SignOut();
+                //refresh
+                Response.Redirect(Request.RawUrl);
+            }
+
+
             //string currentUsersName = HttpContext.Current.User.Identity.Name;
             //FormsIdentity ident = HttpContext.Current.User.Identity as FormsIdentity;
             //if (ident is object)
